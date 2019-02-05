802ce7571ed081bcba15be10541a07c129ae44a7
Danielhoifodt <40982934+Danielhoifodt@users.noreply.github.com>
2019-02-05 15:02:51 +0100

    Building the components

---
+++ src/components/CardEditor.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class CardEditor extends Component {
+  render() {
+    return (
+      <div>
+        This is the editor!
+      </div>
+    );
+  }
+}
+
+export default CardEditor;
---
+++ src/components/CardViewer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class CardViewer extends Component {
+  render() {
+    return (
+      <div>
+        This is the viewer!
+      </div>
+    );
+  }
+}
+
+export default CardViewer;
